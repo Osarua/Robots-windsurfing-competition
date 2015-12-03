9772cd7926f4c2d4568296e00e7d1b5f817e38b0
Osarua <morro34@hotmail.de>
2015-12-03 21:58:52 +0100

    Ab in die Mitte Menue

--- src/gui/Hauptmenue.java
+++ src/gui/Hauptmenue.java
@@ -1,9 +1,12 @@
 package gui;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * Robots windsurfing competition
@@ -13,11 +16,7 @@ import javafx.scene.layout.Pane;
 public class Hauptmenue {
 	 
 	public Pane hauptmenueErzeugen() {
-		GridPane pane = new GridPane();
-		pane.centerShapeProperty();
-		pane.setPadding(new Insets(5));
-		pane.setHgap(5);
-		pane.setVgap(5);
+		BorderPane pane = new BorderPane();
 		ToggleButton btn = new ToggleButton();
 		btn.setText("Neu");
 		ToggleButton btn2 = new ToggleButton();
@@ -26,10 +25,27 @@ public class Hauptmenue {
 		btn3.setText("Hilfe");
 		ToggleButton btn4 = new ToggleButton();
 		btn4.setText("Beenden");
-		pane.add(btn, 60, 60);
-		pane.add(btn2, 60, 62);
-		pane.add(btn3, 60, 64);
-		pane.add(btn4, 60, 66);
+		VBox topPane = new VBox(1);
+		topPane.getChildren().add(schriftZug());
+		topPane.setAlignment(Pos.BOTTOM_CENTER);
+		topPane.setPadding(new Insets(30));
+		VBox buttonPane = new VBox(4);
+		buttonPane.getChildren().add(btn);
+		buttonPane.getChildren().add(btn2);
+		buttonPane.getChildren().add(btn3);
+		buttonPane.getChildren().add(btn4);
+		buttonPane.setAlignment(Pos.TOP_CENTER);
+        buttonPane.setPadding(new Insets(2));
+        buttonPane.setSpacing(6);
+        VBox pc = new VBox(topPane, buttonPane);
+        pc.setAlignment(Pos.CENTER);
+        pane.setCenter(pc);
 		return pane;
+	}
+	
+	public Label schriftZug() {
+		Label bla = new Label();
+		bla.setText("ROBOTS WINDSURFING COMPETITION");
+		return bla;
 	}
 }
